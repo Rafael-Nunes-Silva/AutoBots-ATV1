726625eb9f565ee6b14c32405462f4b614e861b9
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-03-02 15:06:02 -0300

    Selecionador Documento

---
+++ automanager/src/main/java/com/autobots/automanager/modelo/DocumentoSelecionador.java
@@ -0,0 +1,16 @@
+package com.autobots.automanager.modelo;
+
+import java.util.List;
+
+import com.autobots.automanager.entidades.Documento;
+
+public class DocumentoSelecionador {
+	public Documento selecionar(List<Documento> documentos, long id) {
+		for (Documento doc : documentos) {
+			if (doc.getId() == id) {
+				return doc;
+			}
+		}
+		return null;
+	}
+}
